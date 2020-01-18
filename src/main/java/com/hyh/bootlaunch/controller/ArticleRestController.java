25cdfba2aa64d4254e46e5382528d3a21e18df66
HYH <1256191239@qq.com>
2020-01-18 11:57:36 +0800

    master

---
+++ src/main/java/com/hyh/bootlaunch/controller/ArticleRestController.java
@@ -0,0 +1,56 @@
+package com.hyh.bootlaunch.controller;
+
+import com.hyh.bootlaunch.dao.Article;
+import com.hyh.bootlaunch.model.AjaxResponse;
+import com.hyh.bootlaunch.model.ArticleVO;
+import com.hyh.bootlaunch.service.ArticleRestService;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.Date;
+
+import static org.springframework.web.bind.annotation.RequestMethod.*;
+
+@Slf4j
+@RestController
+@RequestMapping("/rest")
+public class ArticleRestController {
+
+    @Autowired
+    ArticleRestService articleRestService;
+ 
+    //增加一篇Article ，使用POST方法
+    @PostMapping("/article")
+    public AjaxResponse saveArticle(@RequestBody ArticleVO article) {
+//        //因为使用了lombok的Slf4j注解，这里可以直接使用log变量打印日志
+//        log.info("saveArticle：{}",article);
+        return  AjaxResponse.success(articleRestService.saveArticle(article));
+    }
+ 
+    
+    //删除一篇Article，使用DELETE方法，参数是id
+    @DeleteMapping("/article/{id}")
+    public AjaxResponse deleteArticle(@PathVariable Long id) {
+//        log.info("deleteArticle：{}",id);
+        articleRestService.deleteArticle(id);
+        return AjaxResponse.success(id);
+    }
+ 
+     //更新一篇Article，使用PUT方法，以id为主键进行更新
+    @PutMapping("/article/{id}")
+    public AjaxResponse updateArticle(@PathVariable Long id, @RequestBody ArticleVO article) {
+        article.setId(id);
+//        log.info("updateArticle：{}",article);
+        articleRestService.updateArticle(article);
+        return AjaxResponse.success(article);
+    }
+ 
+    //获取一篇Article，使用GET方法
+    @GetMapping("/article/{id}")
+    public AjaxResponse getArticle(@PathVariable Long id) {
+
+        ArticleVO article = articleRestService.getArticle(id);
+        return AjaxResponse.success(article);
+    }
+}
---
+++ src/main/java/com/hyh/bootlaunch/service/ArticleJPARestService.java
@@ -0,0 +1,55 @@
+package com.hyh.bootlaunch.service;
+
+import com.hyh.bootlaunch.dao.Article;
+import com.hyh.bootlaunch.dao.ArticleRepository;
+import com.hyh.bootlaunch.model.ArticleVO;
+import com.hyh.bootlaunch.utils.DozerUtils;
+import org.dozer.Mapper;
+import org.springframework.stereotype.Service;
+
+import javax.annotation.Resource;
+import java.util.List;
+import java.util.Optional;
+
+@Service
+public class ArticleJPARestService implements  ArticleRestService  {
+
+    //将JPA仓库对象注入
+    @Resource
+    private ArticleRepository articleRepository;
+
+    @Resource
+    private Mapper dozerMapper;
+
+    public ArticleVO saveArticle(ArticleVO article) {
+
+        Article articlePO = dozerMapper.map(article,Article.class);
+        articleRepository.save(articlePO);    //保存一个对象到数据库，insert
+
+        return  article;
+    }
+
+    @Override
+    public void deleteArticle(Long id) {
+        articleRepository.deleteById(id);   //根据id删除1条数据库记录
+    }
+
+    @Override
+    public void updateArticle(ArticleVO article) {
+        Article articlePO = dozerMapper.map(article,Article.class);
+        articleRepository.save(articlePO);   //更新一个对象到数据库，仍然使用save方法
+    }
+
+    @Override
+    public ArticleVO getArticle(Long id) {
+        Optional<Article> article = articleRepository.findById(id);  //根据id查找一条数据
+        ArticleVO articleVO = dozerMapper.map(article.get(),ArticleVO.class);
+        return articleVO;
+    }
+
+    @Override
+    public List<ArticleVO> getAll() {
+        List<Article> articleLis = articleRepository.findAll();  //查询article表的所有数据
+        return DozerUtils.mapList(articleLis,ArticleVO.class);
+    }
+}
